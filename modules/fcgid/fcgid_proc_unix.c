apache/httpd-mod_fcgid

commit d5fc68f6aa095e7c16dd525936fef4e0f09c7868

explicitly set the permissions of AF_UNIX sockets, as in mod_cgid r654332, despite logs/fcgidsock/ having tighter permissions already.
http://svn.apache.org/viewvc?view=rev&revision=654332



git-svn-id: https://svn.apache.org/repos/asf/httpd/mod_fcgid/trunk@823191 13f79535-47bb-0310-9956-ffa450edef68

modules/fcgid/fcgid_proc_unix.c
@@ -259,6 +259,17 @@ proc_spawn_process(char *lpszwapper, fcgid_proc_info * procinfo,
         return errno;
     }
 
+    /* IPC directory permissions are safe, but avoid confusion */
+    /* Not all flavors of unix use the current umask for AF_UNIX perms */
+
+    rv = apr_file_perms_set(unix_addr.sun_path, APR_FPROT_UREAD|APR_FPROT_UWRITE|APR_FPROT_UEXECUTE);
+    if (rv != APR_SUCCESS) {
+        ap_log_error(APLOG_MARK, APLOG_CRIT, rv, main_server,
+                     "mod_fcgid: Couldn't set permissions on unix domain socket %s",
+                     unix_addr.sun_path);
+        return rv;
+    }
+
     /* Listen the socket */
     if (listen(unix_socket, DEFAULT_FCGID_LISTENBACKLOG) < 0) {
         ap_log_error(APLOG_MARK, APLOG_ERR, errno, main_server,

commit 78d3089f91de8e6d666a9e601fbb2b2927b99b97

restore compatibility with httpd 2.0/APR 0.9 (broken with r823191) by mapping APR_FPROT_foo to APR 0.9 equivalents
Submitted by: Paul Howarth <paul city-fan.org>
Reviewed and modified slightly by: trawick


git-svn-id: https://svn.apache.org/repos/asf/httpd/mod_fcgid/trunk@828220 13f79535-47bb-0310-9956-ffa450edef68

modules/fcgid/fcgid_proc_unix.c
@@ -23,6 +23,7 @@
 #include <sys/stat.h>
 #define CORE_PRIVATE
 #include "httpd.h"
+#include "apr_version.h"
 #include "apr_thread_proc.h"
 #include "apr_strings.h"
 #include "apr_portable.h"
@@ -47,6 +48,12 @@
 #define ap_unixd_config unixd_config
 // #define ap_unixd_setup_child unixd_setup_child
 // #define ap_unixd_set_global_mutex_perms unixd_set_global_mutex_perms
+#endif
+
+#if APR_MAJOR_VERSION < 1
+#define APR_FPROT_UWRITE        APR_UWRITE
+#define APR_FPROT_UREAD         APR_UREAD
+#define APR_FPROT_UEXECUTE      APR_UEXECUTE
 #endif
 
 #define DEFAULT_FCGID_LISTENBACKLOG 5