b8e8bc88033815727a5a0c799ce5ccd20ad2192d
Guenter Knauf <fuankg@apache.org>
2013-07-06 18:44:24 +0000

    Revert acciedently committed change from r1500305.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/mod_fcgid/trunk@1500307 13f79535-47bb-0310-9956-ffa450edef68

--- modules/fcgid/fcgid_proc_unix.c
+++ modules/fcgid/fcgid_proc_unix.c
@@ -19,8 +19,7 @@
 #include <sys/un.h>
 #include <sys/types.h>
 #include <netinet/tcp.h>        /* For TCP_NODELAY */
-//#include <sys/poll.h>
-#include <poll.h>
+#include <sys/poll.h>
 #include <sys/stat.h>
 #define CORE_PRIVATE
 #include "httpd.h"
